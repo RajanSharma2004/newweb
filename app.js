ad634c662d3aa1645ece4b20d0c2c832a7b4d8cd
Rajan Sarma <148219478+RajanSharma2004@users.noreply.github.com>
2024-09-10 17:15:11 +0545

    sd

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Allow requests from localhost:5173 (or adjust for your frontend's URL)
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://newweb-alpha.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://newweb-alpha.vercel.app'],
   methods: 'GET,POST'
 }));
 
